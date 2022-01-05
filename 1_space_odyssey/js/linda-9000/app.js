fd4918600efdaafb28bf6c8f8ca040f6c18b91e0
Linda Sandaker <linda.m.sandaker@gmail.com>
2022-01-05 20:44:15 +0100

    Did nr 2-3

--- 1_space_odyssey/js/linda-9000/app.js
+++ 1_space_odyssey/js/linda-9000/app.js
@@ -80,17 +80,17 @@ _______/\\\\\________/\\\\\\\\\\\\______/\\\________/\\\______/\\\\\\\\\\\______
           `-._/._/
 
 
- 1. CREATE DROID::::::
+1. CREATE DROID::::::
     Below are all the variables needed to initialize an android into consciousness in its default state.
     a. assign the correct values to the variables below,
     b. ensure to choose the correct values and datatypes
     c. the droid should be built in your likeness (a human, with human features, 2 arms, legs etc)
 */
 
-var isAlive = true; // true
+var isAlive = true;
 var fullName = "Corr" + " Nave";
 var suitColor = "purple";
-var eyes = 2001;
+var eyes = 2;
 var eyeColor = "green";
 var hairColor = "yellow";
 var legs = 2;
@@ -106,66 +106,67 @@ var hasJetPack = true;
 
 console.log('\u{1F916} .-.-. Booting .-.-.');
 console.log(
-    fullName,
-    isAlive,
-    hasJetPack,
-    eyes,
-    eyeColor,
-    hairColor,
-    legs,
-    arms,
-    hands,
-    ears,
-    canWalk,
-    canFly,
-    hasWeapon
+  fullName,
+  isAlive,
+  isInGoodMood,
+  hasJetPack,
+  eyes,
+  eyeColor,
+  hairColor,
+  suitColor,
+  legs,
+  arms,
+  hands,
+  ears,
+  canWalk,
+  canRun,
+  canFly,
+  hasWeapon
 );
 
 /*
- 2. BROKEN SATELLITE::::::
+2. BROKEN SATELLITE::::::
     You need to send the droid out on a space walk to fix the satellite, with out it you can't communicate with earth.
     a) write an if condition checking if the jet pack is enabled, if true the droid can go outside if false the droid
-       cannot go outside, console log the correct log based on that condition.
+      cannot go outside, console log the correct log based on that condition.
 */
 console.log('\u{1F6F0} BROKEN SATELLITE::::::');
 
 if (hasJetPack) {
-    console.log("JetPack is enabled!!! off I go outside to fix the satellite");
+  console.log("JetPack is enabled!!! off I go outside to fix the satellite");
 } else {
-    console.log("JetPack NOT enabled, please enable jetPack before doing a space walk");
+  console.log("JetPack NOT enabled, please enable jetPack before doing a space walk");
 }
 
 /*
- 3. METEOR SHOWER::::::
+3. METEOR SHOWER::::::
     On your way to Jupiter you encounter a massive meteor shower that could damage the ship, you need to redirect the ship to a new safe location.
     a) Using a for loop iterate through the responses array and choose the appropriate response to get out of danger.
       create an if condition in your loop to print only 1 response using a comparison operator.
-
+*/
 console.log('\u{1F327} METEOR SHOWER::::::');
 
 var responses = [
-    'Call basecamp and ask for assistance, but they are far away',
-    'Do nothing, just push through',
-    'initiate core thrusters, punch 180degrees into dash-nav and warp away from danger',
-    'ask humanoid-9000 to fly out there and shield the ship from the debris, this action could harm the humanoid',
+  "Call basecamp and ask for assistance, but they are far away",
+  "Do nothing, just push through",
+  "Invalid core thrusters, punch 180degrees into dash-nav and warp away from danger",
+  "Ask humanoid-9000 to fly out there and shield the ship from the debris, this action could harm the humanoid"
 ];
 
 for (var i = 0; i <= responses.length; i++) {
-    if (responses[i] === responses[3]) {
-        console.log(responses[i]);
-    }
+  if (responses[i] === responses[3]) {
+    console.log(responses[i]);
+  }
 }
-*/
 
-// write your code here, use the console log beneath
-// console.log("\u{1F6F8} Swoosh -> -> Ship is steered to safety!");
+console.log("\u{1F6F8} Swoosh -> -> Ship is steered to safety!");
 
 /*
- 4. ALIENS SPACE CRAFT ENCROACHING::::::
+4. ALIENS SPACE CRAFT ENCROACHING::::::
     Alien craft approaches its an enemy for sure...you need to shoot it down!!
     a) convert the pseudoscope to real code
-       create the required variables below and fill in the correct values and dataTypes to make
-       the if statements pass so that photonLaser fires pew pew pew.
+      create the required variables below and fill in the correct values and dataTypes to make
+      the if statements pass so that photonLaser fires pew pew pew.
 
 console.log('\u{1F47E} ALIENS SPACE CRAFT ENCROACHING::::::');
 
